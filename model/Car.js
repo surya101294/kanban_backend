2b4fef9fb1cb12e741e97e133058cdd1d2e036a4
surya <surya.prakash@sutraatechno.com>
2023-10-02 16:35:14 +0530

    car schema change

--- model/Car.js
+++ model/Car.js
@@ -11,6 +11,10 @@ const carSchema = new mongoose.Schema({
   fuel: { type: String, required: true },
   price: { type: Number, required: true },
   image: { type: String, required: true },
+  image2: { type: String, required: true },
+  image3: { type: String, required: true },
+  image4: { type: String, required: true },
+  image5: { type: String, required: true }
 }, {
   versionKey: false,
 });
