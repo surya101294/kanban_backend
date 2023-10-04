f2b29f1f734c171e5f5532b405baf6d1cc1cd100
surya <surya.prakash@sutraatechno.com>
2023-10-04 22:09:07 +0530

    car schema change

--- model/Car.js
+++ model/Car.js
@@ -2,10 +2,11 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
+  
   year: { type: Number, required: true },
   make: { type: String, required: true },
   model: { type: String, required: true },
-  kms: { type: String, required: true },
+  kms: { type: Number, required: true },
   transmission: { type: String, required: true },
   exteriorcolor: { type: String, required: true },
   fuel: { type: String, required: true },
@@ -14,9 +15,12 @@ const carSchema = new mongoose.Schema({
   image2: { type: String, required: true },
   image3: { type: String, required: true },
   image4: { type: String, required: true },
-  image5: { type: String, required: true }
+  image5: { type: String, required: false },
+  image6: { type: String, required: false },
+  booked: { type: String, required: false }
 }, {
   versionKey: false,
 });
 
 module.exports = mongoose.model('Car', carSchema);
+
